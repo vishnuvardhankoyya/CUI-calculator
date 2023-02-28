dfd8711d1f3fd4fdb3f6efa7c31fc6b85d0ea71e
tejavarddhank <kodalitejavarddhan@gmail.com>
2023-02-28 13:59:52 +0530

    added division code

--- final calculator.py
+++ final calculator.py
@@ -1,17 +1,16 @@
-def add(number1, number2): 
-    return (number1 + number2) 
+def divide(number1, number2): 
+    return (number1 / number2)
 
-print("Operation Menu -\n" \ 
-        "1. Addition of two numbers\n" \ )
+print("Operation Menu -\n" \  
+        "4. Division of two numbers\n" \)
 
 select = input("Select operations form 1, 2, 3, 4 :") 
   
 num1 = int(input("Enter first number: ")) 
-num2 = int(input("Enter second number: "))
+num2 = int(input("Enter second number: ")) 
 
-if select == '1': 
-    print(num1, "+", num2, "=", 
-                    add(num1, num2)) 
-    
+if select == '4': 
+    print(num1, "/", num2, "=", 
+                    divide(num1, num2)) 
 else: 
-    print("Invalid input!")
+    print("Invalid input!")
